82f2b509d2cd0352cc3430a11e3a69ee7e9c6703
Seo Sang won <33479927+zhaoSeo@users.noreply.github.com>
2018-05-14 15:46:27 +0900

    create tableview label slider

--- UISlider TableView/ViewController.swift
+++ UISlider TableView/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    @IBOutlet weak var mySlider: UISlider!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTableView.dataSource = self
+        myTableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 101
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "RE"
+        let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        //재활용
+        let tableView = indexPath.row
+        cell.textLabel?.text = String(tableView)
+        //cell.textLabel?.text = String(Int(mySlider.value) + indexPath.row)
+        //title
+        return cell
+    }
+    @IBAction func Slidermoved(_ sender: UISlider) {
+        myLabel.text = String(Int(mySlider.value))
+        let indexPath = IndexPath(row: Int(myLabel.text!)!, section:0)
+        myTableView.scrollToRow(at: indexPath, at: UITableViewScrollPosition.top, animated: true)
+        //myTableView.reloadData()
+    }
+    
 
 }
 
